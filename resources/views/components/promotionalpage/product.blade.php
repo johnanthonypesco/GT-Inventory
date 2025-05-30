6c80b34e5965d423b0a34e6a9591ef78acd2dc18
John Anthony Pesco <pescojohnanthony@gmail.com>
2025-05-31 01:21:16 +0800

    finish the functionality in promotional page except for the inquire now page

--- resources/views/components/promotionalpage/product.blade.php
+++ resources/views/components/promotionalpage/product.blade.php
@@ -5,8 +5,10 @@
     'form' => '',
 ])
 
-<div class="bg-white rounded-lg flex flex-col items-center justify-center p-5 mt-10 !w-80" style="box-shadow: 0 0 5px black;">
-    <img src="{{asset ('image/'. $image)}}" alt="Ceftriaxone" class="w-[120px] h-[120px]">
+<div class="bg-white rounded-lg flex flex-col items-center justify-center p-5 mt-5 !w-60 product-card"
+     style="box-shadow: 0 0 5px black;"
+     data-type="{{ strtolower($form) }}">
+    <img src="{{ asset('image/' . $image) }}" alt="{{$genericname}}" class="w-[120px] h-[120px]">
     <h1 class="mt-5 font-bold text-xl text-[#084876]">{{$genericname}}</h1>
     <p class="font-semibold text-lg text-[#084876]">{{$brandname}}</p>
     <p class="font-semibold">{{$form}}</p>
