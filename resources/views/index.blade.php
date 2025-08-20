2b54760c8791b1fd92f9fdacee82b0be50b8a5c4
John Anthony Pesco <pescojohnanthony@gmail.com>
2025-08-20 15:48:27 +0800

    add animation to the landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
             <a href="#home" class="lg:mr-12 font-semibold hover:text-[#0097D3]">Home</a>
             <a href="#about" class="lg:mr-12 font-semibold hover:text-[#0097D3]">About Us</a>
             <a href="#products" class="lg:mr-12 font-semibold hover:text-[#0097D3]">Products</a>
-            <button onclick="window.location.href = '#inquire'" class="bg-[#0097D3] w-fit px-5 py-2 rounded-lg font-semibold text-white">Reach Us</button>
+            <button id="inquire" onclick="window.location.href = '#inquire'" class="bg-[#0097D3] w-fit px-5 py-2 rounded-lg font-semibold text-white hover:bg-[#005382] hover:cursor-pointer hover:-translate-y-1 transition-all duration-200">Reach Us</button>
             
             {{-- <a href="{{ route('beta.register') }}" class="bg-red-600 animate-pulse w-fit px-5 py-2 rounded-lg font-semibold text-white">Join The Beta Test Program</a> --}}
         </nav>
